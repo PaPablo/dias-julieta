7305f0345e35c7e047a4cb529101c3a16c712ff2
Pablo Toledo Margalef <pabloatm980@gmail.com>
2021-09-13 23:32:00 -0300

    Sitio creado

---
+++ index.js
@@ -0,0 +1,27 @@
+const setContador = () => {
+  const hoy = new Date();
+  const llegada = new Date(2021, 8, 25, 17);
+
+  const diff = Math.floor((llegada - hoy) / 1000);
+
+  const dias = Math.floor(diff / 3600 / 24);
+  const horas = Math.floor((diff / 3600) % 24);
+  const minutos = Math.floor(diff / 60) % 60;
+  const segundos = diff % 60;
+
+  const diasSpan = document.querySelector(".dias");
+  const horasSpan = document.querySelector(".horas");
+  const minutosSpan = document.querySelector(".minutos");
+  const segundosSpan = document.querySelector(".segundos");
+
+  diasSpan.innerHTML = `${dias}`;
+  horasSpan.innerHTML = `${horas}`;
+  minutosSpan.innerHTML = `${minutos}`;
+  segundosSpan.innerHTML = `${segundos}`;
+};
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  setContador();
+});
+
+setInterval(setContador, 1000);
